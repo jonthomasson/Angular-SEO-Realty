69368ba57dc9a61803c42211cef6624d67b33bf5
Jon Thomasson <jthomasson@sjcoe.net>
2023-08-23 16:14:02 -0700

    adding meta description and robots.txt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,13 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'seo-realty';
+
+  constructor(private metaService: Meta) { }
+
+  ngOnInit(): void {
+    this.metaService.addTag({ name: 'description', content: 'Discover real estate with ease. Leveraging advanced SEO techniques, our platform offers a seamless property search experience that gets you closer to your dream home.' });
+  }
 }
